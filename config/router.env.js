d33ad72d3cd82d405a614674974d49c184c648da
zhangkun <1766597067@qq.com>
2018-04-28 17:03:33 +0800

    重新打包

--- config/router.env.js
+++ config/router.env.js
@@ -2,6 +2,6 @@
  * Created by administrato on 2017/7/7.
  */
 module.exports = {
-  baseVersionRouter: '/v1/', // 有版本
+  baseVersionRouter: '/vue-component/dist/', // github pages配置
   baseRouter: '/', // 没有版本和熱加載用的
 };
